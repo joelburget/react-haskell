bc7c385a27bad0fea984ccf27f0dd4fd202c80be
Joel Burget <joel@khanacademy.org>
2015-01-02 14:11:15 -0800

    Clean up export list.

--- src/React.hs
+++ src/React.hs
@@ -1,16 +1,18 @@
 module React
     ( module X
 
+    -- React.Anim
+    , Easing(..)
+    , Color(..)
+    , getAnimationState
+    , Animatable(..) -- XXX
+
     -- React.Class
     , ReactClass()
     , createClass
 
-    -- React.Anim
-    , Easing(..)
-    , Color(..)
-    , interpolate
-    , getAnimationState
-    , Animatable(..) -- XXX
+    -- React.Local
+    , locally
 
     -- React.Render
     , cancelRender
@@ -23,12 +25,13 @@ module React
 -- * escaping / dangerouslySetInnerHTML
 
 import React.Anim
-import React.Attrs as X
 import React.Class
+-- import React.Imports
+-- import React.Interpret
+import React.Local
+import React.Render
+
+import React.Attrs as X
 import React.Elements as X
 import React.Events as X
-import React.Imports
-import React.Interpret
-import React.Local as X
-import React.Render
 import React.Types as X
