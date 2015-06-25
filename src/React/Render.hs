78ab42c2ad38c2cfecb978f459218639a8d50d46
Joel Burget <joelburget@gmail.com>
2015-06-25 15:46:45 +0800

    Greatly simplify the React.Render import list.

--- src/React/Render.hs
+++ src/React/Render.hs
@@ -5,21 +5,9 @@ module React.Render
     , debugRender
     ) where
 
-import Control.Applicative
-import Control.Monad
-import Control.Monad.IO.Class
-import Data.Functor.Identity
-import Data.IORef
-import Data.List
-import Data.Maybe
-import Data.Monoid
-import Data.String
 import Data.Void
 
-import React.Attrs
-import React.Class
-import React.Elements
-import React.Events
+import React.GHCJS
 import React.Imports
 import React.Interpret
 import React.Types
